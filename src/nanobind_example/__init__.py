8acc1dd9059d832b11fa88f60c1d77d70f8d0b31
Ian Bell <ian.bell@nist.gov>
2024-06-10 19:45:34 -0400

    Show show to set .doc() in extension and have that get pulled in properly

--- src/nanobind_example/__init__.py
+++ src/nanobind_example/__init__.py
@@ -1,1 +1,2 @@
 from .nanobind_example_ext import add
+from .nanobind_example_ext import __doc__
--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -5,5 +5,6 @@ namespace nb = nanobind;
 using namespace nb::literals;
 
 NB_MODULE(nanobind_example_ext, m) {
+    m.doc() = "This is a \"hello world\" example with nanobind";
     m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
 }
