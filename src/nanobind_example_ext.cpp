2ee3ccab34a4a8a357413c661e9f9480ac044e60
duke <duke@MacBook-Pro-de-sophie.local>
2024-12-27 16:01:34 +0100

    test actions

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -7,4 +7,5 @@ using namespace nb::literals;
 NB_MODULE(nanobind_example_ext, m) {
     m.doc() = "This is a \"hello world\" example with nanobind";
     m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+    m.def("mul", [](int a, int b) { return a * b; }, "a"_a, "b"_a);
 }
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,3 +3,6 @@ import nanobind_example as m
 def test_add():
     assert m.add(1, 2) == 3
     assert m.add(1, 3) == 3
+
+def test_mul():
+    assert m.mul(2,3) == 6
